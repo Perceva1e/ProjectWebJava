24622305756d13508b2c6ff5f4a8d0ad0a88a158
Percevale <denis_shagun@mail.ru>
2024-03-22 20:05:38 +0300

    updates

--- src/main/java/bsuir/group/projectweb/aop/AuthorAspect.java
+++ src/main/java/bsuir/group/projectweb/aop/AuthorAspect.java
@@ -23,11 +23,9 @@ public class AuthorAspect {
             for (Object arg : arguments) {
                 if (arg instanceof Long id) {
                     if (countArg == 1) {
-                        id = (Long) arg;
                         log.info("Try delete connection author in text with id {}", id);
                         break;
                     }
-                    id = (Long) arg;
                     log.info("Try delete connection author with id {}", id);
                     countArg++;
                 }
@@ -47,12 +45,12 @@ public class AuthorAspect {
     @Around("PointCuts.saveMethodsAuthor()")
     public Object aroundSaveAdvice(ProceedingJoinPoint joinPoint) {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Author author = null;
+        Author authors = null;
         if (methodSignature.getName().equals("savePerson")) {
             Object[] arguments = joinPoint.getArgs();
             for (Object arg : arguments) {
-                if (arg instanceof Author) {
-                    author = (Author) arg;
+                if (arg instanceof Author author) {
+                    authors = author;
                     log.info("Try add author with name {}", author.getFirstName());
                 }
             }
@@ -64,20 +62,21 @@ public class AuthorAspect {
             log.error(e.getMessage(), e);
             result = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        assert author != null;
-        log.info("author with name {} add", author.getFirstName());
+        assert authors != null;
+        log.info("author with name {} add", authors.getFirstName());
         return result;
     }
 
     @Around("PointCuts.findMethodsAuthor()")
     public Object aroundFindAdvice(ProceedingJoinPoint joinPoint) {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        String information = null;
+        String informations = null;
         if (methodSignature.getName().equals("findAuthorByParameters")) {
             Object[] arguments = joinPoint.getArgs();
             for (Object arg : arguments) {
-                if (arg instanceof String) {
+                if (arg instanceof String information) {
                     information = (String) arg;
+                    informations = information;
                     log.info("Try find all author with lastname {}", information);
                 }
             }
@@ -91,7 +90,7 @@ public class AuthorAspect {
             result = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
         if (methodSignature.getName().equals("findAuthorByParameters")) {
-            log.info("Method find all author with lastname {}", information);
+            log.info("Method find all author with lastname {}", informations);
         }
         return result;
     }
@@ -99,12 +98,10 @@ public class AuthorAspect {
     @Around("PointCuts.addMethodsAuthor()")
     public Object aroundAddAdvice(ProceedingJoinPoint joinPoint) {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Author author;
         if (methodSignature.getName().equals("addAuthorInText")) {
             Object[] arguments = joinPoint.getArgs();
             for (Object arg : arguments) {
-                if (arg instanceof Author) {
-                    author = (Author) arg;
+                if (arg instanceof Author author) {
                     log.info("Try add author with name {}", author.getFirstName());
                 }
             }
