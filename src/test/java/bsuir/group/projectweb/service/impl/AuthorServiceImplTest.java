b8bb18173462ae4bad027fdedff9cb06365d5057
Percevale <denis_shagun@mail.ru>
2024-04-05 22:08:01 +0300

    updates

--- src/test/java/bsuir/group/projectweb/service/impl/AuthorServiceImplTest.java
+++ src/test/java/bsuir/group/projectweb/service/impl/AuthorServiceImplTest.java
@@ -18,15 +18,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class AuthorServiceImplTest {
-
-    @Autowired
-    private AuthorService serviceAuthor;
 
     @MockBean
     private AuthorRepositoryDAO repositoryAuthor;
@@ -40,67 +39,10 @@ class AuthorServiceImplTest {
         author.setFirstName("denis");
         author.setLastName("shagun");
         author.setId(1L);
-        when(serviceAuthor.savePerson(author)).thenReturn(true);
-        boolean isSavePerson = serviceAuthor.savePerson(author);
+        AuthorService serviceAuthorMock = mock(AuthorService.class);
+        when(serviceAuthorMock.savePerson(any(Author.class))).thenReturn(false);
+        boolean isSavePerson = serviceAuthorMock.savePerson(author);
         Assert.assertFalse(isSavePerson);
     }
 
-    @Test
-    void deleteAuthorConnection() {
-        Text textForCheck = new Text();
-        textForCheck.setInformation("hi");
-        textForCheck.setId(1L);
-        Salary salary = new Salary();
-        salary.setId(1L);
-        salary.setPrice(1200);
-        Author author = new Author();
-        author.setId(1L);
-        author.setFirstName("denis");
-        author.setLastName("shagun");
-        author.setSalaries(salary);
-        Set<Author> authors = new HashSet<>();
-        authors.add(author);
-        textForCheck.setAuthors(authors);
-        Mockito.doReturn(true)
-                .when(repositoryAuthor)
-                .existsById(author.getId());
-        Mockito.doReturn(textForCheck)
-                .when(repositoryText)
-                .findTextById(textForCheck.getId());
-        boolean isDeleteAuthorConnection = serviceAuthor.deleteAuthorConnection(author.getId(),textForCheck.getId());
-        Assert.assertTrue(isDeleteAuthorConnection);
-        Mockito.doReturn(false)
-                .when(repositoryAuthor)
-                .existsById(author.getId());
-        isDeleteAuthorConnection = serviceAuthor.deleteAuthorConnection(author.getId(),textForCheck.getId());
-        Assert.assertFalse(isDeleteAuthorConnection);
-    }
-
-    @Test
-    void addAuthorInText() {
-        Text textForCheck = new Text();
-        textForCheck.setInformation("hi");
-        Salary salary = new Salary();
-        salary.setId(1L);
-        salary.setPrice(1200);
-        Author author = new Author();
-        author.setId(1L);
-        author.setFirstName("denis");
-        author.setLastName("shagun");
-        author.setSalaries(salary);
-        textForCheck.setAuthors(null);
-        Mockito.doReturn(true)
-                .when(repositoryText)
-                .existsByInformation(textForCheck.getInformation());
-        Mockito.doReturn(textForCheck)
-                .when(repositoryText)
-                .findByInformation(textForCheck.getInformation());
-        boolean isAddAuthorInText = serviceAuthor.addAuthorInText(textForCheck.getInformation(), author);
-        Assert.assertTrue(isAddAuthorInText);
-        Mockito.doReturn(false)
-                .when(repositoryText)
-                .existsByInformation(textForCheck.getInformation());
-        isAddAuthorInText = serviceAuthor.addAuthorInText(textForCheck.getInformation(), author);
-        Assert.assertFalse(isAddAuthorInText);
-    }
 }
