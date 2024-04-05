078cae16714834249e2664b547340669964db578
Percevale <denis_shagun@mail.ru>
2024-04-05 22:59:58 +0300

    updates

--- src/test/java/bsuir/group/projectweb/service/impl/SalaryServiceImplTest.java
+++ src/test/java/bsuir/group/projectweb/service/impl/SalaryServiceImplTest.java
@@ -6,24 +6,28 @@ import bsuir.group.projectweb.repository.AuthorRepositoryDAO;
 import bsuir.group.projectweb.repository.SalaryRepositoryDAO;
 import bsuir.group.projectweb.service.SalaryService;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.assertj.core.api.Assertions.assertThat;
 
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class SalaryServiceImplTest {
 
-    @Autowired
-    private SalaryService serviceSalary;
-    @MockBean
+    @InjectMocks
+    private SalaryServiceImpl serviceSalary;
+
+    @Mock
     private SalaryRepositoryDAO repositorySalary;
-    @MockBean
+
+    @Mock
     private AuthorRepositoryDAO repositoryAuthor;
 
     @Test
@@ -32,50 +36,5 @@ class SalaryServiceImplTest {
         salary.setId(1L);
         boolean isSaveSalary = serviceSalary.saveSalary(salary);
         Assert.assertFalse(isSaveSalary);
-    }
-
-    @Test
-    void deleteSalary() {
-        Salary salary = new Salary();
-        salary.setId(1L);
-        Mockito.doReturn(true)
-                .when(repositorySalary)
-                .existsById(salary.getId());
-        Mockito.doReturn(salary)
-                .when(repositorySalary)
-                .findSalariesById(salary.getId());
-        Mockito.doReturn(new Author())
-                .when(repositoryAuthor)
-                .findAuthorBySalaries(salary);
-        boolean isDeleteSalary = serviceSalary.deleteSalaryInAuthor(salary.getId());
-        Assert.assertTrue(isDeleteSalary);
-        Mockito.doReturn(false)
-                .when(repositorySalary)
-                .existsById(salary.getId());
-        isDeleteSalary = serviceSalary.deleteSalaryInAuthor(salary.getId());
-        Assert.assertFalse(isDeleteSalary);
-    }
-
-    @Test
-    void changeSalaryById() {
-        Salary salary = new Salary();
-        salary.setId(1L);
-        salary.setPrice(12000);
-        Author author = new Author();
-        author.setId(1L);
-        author.setSalaries(salary);
-        Mockito.doReturn(true)
-                .when(repositoryAuthor)
-                .existsById(author.getId());
-        Mockito.doReturn(author)
-                .when(repositoryAuthor)
-                .findAuthorById(author.getId());
-        boolean isChangeSalaryById = serviceSalary.changeSalaryByIdInAuthor(author.getId(), salary.getPrice());
-        Assert.assertTrue(isChangeSalaryById);
-        Mockito.doReturn(false)
-                .when(repositoryAuthor)
-                .existsById(author.getId());
-        isChangeSalaryById = serviceSalary.changeSalaryByIdInAuthor(author.getId(), salary.getPrice());
-        Assert.assertFalse(isChangeSalaryById);
     }
 }
