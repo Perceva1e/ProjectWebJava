1ecde0a927a7ba6aed2a7d5ba73f814566d2db1b
Percevale <denis_shagun@mail.ru>
2024-04-06 15:41:43 +0300

    more test

--- src/test/java/bsuir/group/projectweb/service/impl/CounterServiceImplTest.java
+++ src/test/java/bsuir/group/projectweb/service/impl/CounterServiceImplTest.java
@@ -1,28 +1,26 @@
 package bsuir.group.projectweb.service.impl;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class CounterServiceImplTest {
 
     @Test
     void enhanceCounter() {
+        CounterServiceImpl.enhanceCounter();
+        Assertions.assertEquals(1, CounterServiceImpl.getCounter());
     }
 
     @Test
     void getCounter() {
-    }
-
-    @Test
-    void testEquals() {
-    }
-
-    @Test
-    void testHashCode() {
-    }
-
-    @Test
-    void testToString() {
+        CounterServiceImpl.enhanceCounter();
+        CounterServiceImpl.enhanceCounter();
+        int counter = CounterServiceImpl.getCounter();
+        Assertions.assertEquals(3, counter);
     }
 }
