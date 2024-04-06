2ec6a9973ef6630c59594dd6e9afdf97adb10d87
Percevale <denis_shagun@mail.ru>
2024-04-06 16:08:01 +0300

    updates

--- src/test/java/bsuir/group/projectweb/cache/TextDataCacheTest.java
+++ src/test/java/bsuir/group/projectweb/cache/TextDataCacheTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @ExtendWith(MockitoExtension.class)
 class TextDataCacheTest {
     private final TextDataCache textCache = new TextDataCache();
@@ -32,5 +35,30 @@ class TextDataCacheTest {
         textCache.putText(key, value);
         textCache.clearText();
         Assertions.assertNull(textCache.getText(key));
+    }
+    @Test
+    public void testGetterAndSetter() {
+        TextDataCache cache = new TextDataCache();
+        Map<String, Object> map = new ConcurrentHashMap<>();
+        map.put("key", "value");
+        cache.setHashMap(map);
+
+        Assertions.assertEquals(map, cache.getHashMap());
+    }
+
+    @Test
+    public void testEqualsAndHashCode() {
+        TextDataCache cache1 = new TextDataCache();
+        Map<String, Object> map1 = new ConcurrentHashMap<>();
+        map1.put("key", "value");
+        cache1.setHashMap(map1);
+
+        TextDataCache cache2 = new TextDataCache();
+        Map<String, Object> map2 = new ConcurrentHashMap<>();
+        map2.put("key", "value");
+        cache2.setHashMap(map2);
+
+        Assertions.assertEquals(cache1, cache2);
+        Assertions.assertEquals(cache1.hashCode(), cache2.hashCode());
     }
 }
